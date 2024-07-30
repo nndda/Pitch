9d987989aa0bdaa2da477d0e6eda64a2b3d666e6
nnda <96333146+nndda@users.noreply.github.com>
2024-07-31 03:37:04 +0800

    Refactor & improve readability

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,126 +1,170 @@
 import "./styles/main.scss";
+
 import {
-  // components,
+  components,
   PitchComponentData,
   PitchComponentsLibrary,
   PitchComponentsCollection,
 } from "./scripts/components";
+
 import { compileComponents } from "./scripts/compile";
-import { copyNotif } from "./scripts/copy";
+import { copyNotif, copyString } from "./scripts/copy";
+import { highlightHTML } from "./scripts/highlighter";
 
 const d = document;
-let compInputs : Array<HTMLInputElement> = [];
 
-const compSelectAllBtn = d.getElementById("btn-select-all");
-const compSelectNoneBtn =  d.getElementById("btn-select-none");
+const compList = $("#components-list");
 
 const compileCompBtn = <HTMLInputElement>d.getElementById("compile-components-btn");
 
 const compTitle = d.getElementById("component-title");
 const compDesc = d.getElementById("component-description");
 
-const compPreviewSelector = <HTMLSelectElement>d.getElementById("component-info-selector");
-const compPreview = d.getElementById("component-preview");
+const compPreview = $("#component-preview");
 
 let componentsData : PitchComponentsCollection;
 
 fetch("./components.json")
   .then(response => response.text())
-  .then(data => {
-    componentsData = JSON.parse(data);
-    // console.log(componentsData);
-    initializeComponents();
-  })
+  .then(data => {componentsData = JSON.parse(data);})
   .catch(err => {throw err});
 
+let compGroups : Array<String> = []
+
 function initializeComponents() {
-  let compPreviewCSS = d.createElement("style");
+  for (const comp in components) {
+    let compData = components[comp];
 
-  for (const comp in componentsData) {
-    let compData = componentsData[comp];
-
-    if (compData.name !== "variables") {
-      const compName = compData.name.replace(/\-/g, " ");
+    if (comp !== "variables") {
+      const compName = comp.replace(/\-/g, " ");
       const compID = "comp-" + comp;
-      const compInput = d.createElement("input");
-      [
-        ["type", "checkbox"],
-        ["name", "component-toggle"],
-        ["id", compID],
-        ["data-comp", comp],
-      ].forEach(attr => {
-        compInput.setAttribute(attr[0], attr[1]);
+
+      if (!compGroups.includes(compData["type"])) {
+        compGroups.push(compData["type"]);
+        const compElemGroup = $(`
+          <dt>
+            <span class="component-type-title">
+              ${compData["type"]}
+            </span>
+
+            <button class="button-general component-select-all">
+              <span class="btn-icon material-symbols-outlined">
+                grid_view
+              </span>
+            </button>
+
+            <button class="button-general component-select-none">
+              <span class="btn-icon material-symbols-outlined">
+                square
+              </span>
+            </button>
+
+          </dt>
+        `);
+        compElemGroup.on("click", ".component-select-all", function() {
+          d.querySelectorAll(`[data-type="${compData["type"]}"]`).forEach(function(el : HTMLInputElement) {
+            el.checked = true;
+          });
+          calculateComponents();
+        });
+        compList.append(compElemGroup);
+      }
+
+      const compElemItem = $(`
+        <dd data-search="${compName}">
+          <input
+            type="checkbox"
+            name="component-toggle"
+            id="${compID}"
+            data-comp="${comp}"
+            data-type="${compData["type"]}"
+          >
+
+          <label for="${compID}">
+            <i class="fa-solid fa-square-check checked-not"></i>
+            <i class="fa-regular fa-square checked"></i>
+          </label>
+
+          <button class="component-toggle">
+            ${compName}
+          </button>
+        </dd>
+      `);
+
+      compElemItem.on("input", "input", function() {
+        calculateComponents();
       });
 
-      const compLabel = d.createElement("label");
-      compLabel.setAttribute("for", compID);
-      compLabel.setAttribute("class", "component-toggle");
-      compLabel.textContent = compName;
-
-      compInput.addEventListener("input", () => {calculateComponents()});
-
-      compLabel.addEventListener("mouseover", () => {
-        compPreviewSelector.value = compInput.getAttribute("data-comp");
-        setCompInfo(compPreviewSelector.value);
+      compElemItem.on("click", "button.component-toggle", function() {
+        setCompInfo(comp);
       });
 
-      d.getElementById("components-toggles").appendChild(compInput);
-      d.getElementById("components-toggles").appendChild(compLabel);
-
-      compInputs.push(compInput);
-
-      const compInfoOpt = d.createElement("option");
-      compInfoOpt.setAttribute("value", comp);
-      compInfoOpt.innerText = compName;
-
-      if (compName === "accordion") compInfoOpt.setAttribute("selected", "");
-
-      compPreviewSelector.appendChild(compInfoOpt);
-
-      compPreviewCSS.innerText += compData.css;
+      compList.append(compElemItem);
     }
   }
-  compPreviewSelector.addEventListener("input", () => {
-    setCompInfo(compPreviewSelector.value);
-  });
-
-  document.head.appendChild(compPreviewCSS);
 }
+
+const homeButton = $("#home-button");
+const homeContent = $("#home-content");
+
+homeButton.on("click", function() {
+  homeContent.toggleClass("hidden", false);
+  homeButton.toggleClass("hidden", true);
+  compPreview.html("");
+  compDesc.textContent = "";
+  compTitle.textContent = "Pitch";
+});
 
 function setCompInfo(comp : string) {
-  compTitle.textContent = componentsData[comp].name.replace(/\-/g, " ");
-  compDesc.textContent = componentsData[comp].desc;
-  compPreview.innerHTML = componentsData[comp].sampleHTML;
+  compTitle.textContent = comp.replace(/\-/g, " ");
+  compDesc.textContent = components[comp].desc;
+  compPreview.off("click");
+  compPreview.html("");
+
+  for (const n in components[comp].sampleHTML) {
+    compPreview.append($(`
+      <div class="component-container-single">
+        <div class="component-display">
+          ${components[comp].sampleHTML[n]}
+        </div>
+
+        <details class="code-collapse">
+          <summary class="button-general">
+            <i class="fa-solid fa-code"></i> &nbsp;
+            Show HTML
+          </summary>
+          <button class="comp-copy button-general" data-html-id="${n}">
+            <i class="fa-solid fa-copy"></i>
+            Copy
+          </button>
+        </details>
+
+        <div class="component-html">
+          <pre><code>${highlightHTML(components[comp].sampleHTML[n])}</code></pre>
+        </div>
+      </div>
+      <br>
+    `));
+    compPreview.on("click", ".comp-copy", function() {
+      copyString(components[comp].sampleHTML[<number>(<unknown>($(this).attr("data-html-id")))]);
+    });
+  }
+
+
+  homeButton.toggleClass("hidden", false);
+  homeContent.toggleClass("hidden", true);
 }
-
-compSelectAllBtn.addEventListener("click", () => {
-  for (let n = compInputs.length - 1; n >= 0; n--) {
-    compInputs[n].checked = true;
-  }
-  calculateComponents();
-});
-
-compSelectNoneBtn.addEventListener("click", () => {
-  for (let n = compInputs.length - 1; n >= 0; n--) {
-    compInputs[n].checked = false;
-  }
-  calculateComponents();
-});
-
 
 compileCompBtn.addEventListener("click", () => {
   let selectedComps : Array<string> = [];
-  compInputs.forEach(inp => {
-    if (inp.checked)
-      selectedComps.push(inp.getAttribute("data-comp"));
+
+  d.querySelectorAll("input[name='component-toggle']:checked")
+    .forEach(function(el) {
+      selectedComps.push(el.getAttribute("data-comp"));
   });
+
   compileComponents(selectedComps, componentsData);
 });
-calculateComponents();
-
-function getCompScope(): string {
-  return (<HTMLInputElement>d.querySelector("input[name='']:checked")).value;
-}
 
 function calculateComponents() {
   const compSelected = d.querySelectorAll("input[name='component-toggle']:checked");
@@ -131,3 +175,9 @@ function calculateComponents() {
 
   }
 }
+calculateComponents();
+
+initializeComponents();
+
+import "./scripts/search";
+// initializeSearchFunction();
