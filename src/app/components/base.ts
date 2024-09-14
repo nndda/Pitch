db2e4725291bc39cfac98c105e11a482fe16c366
nnda <96333146+nndda@users.noreply.github.com>
2024-09-15 03:58:10 +0800

    Resolve vulnerabilities

--- src/app/components/base.ts
+++ src/app/components/base.ts
@@ -24,10 +24,83 @@ export const compBase : PitchComponentsLibrary = {
 
 </div>`
 ,
+`
+<div class="custom-accrd">
+
+    <p>
+      This asset pack is licensed under 
+      <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">
+        CC BY 4.0
+      </a>
+      <img src="https://mirrors.creativecommons.org/presskit/icons/cc.svg" width="20px">
+      <img src="https://mirrors.creativecommons.org/presskit/icons/by.svg" width="20px">
+    </p>
+
+  <details>
+    <summary>What you can and can't do</summary>
+      <ul>
+        <li>
+          Use in commercial or non-commercial projects
+        </li>
+        <li>
+          Modify, or adapt to your needs
+        </li>
+        <li>
+          Redistribute and share
+          <ul>
+            <li>though, plz redistribute them as a part of a project, and not as an asset pack by itself</li>
+          </ul>
+        </li>
+        <li>
+          No AI or NFTs
+        </li>
+      </ul>
+  </details>
+
+  <details>
+    <summary>How to give attribution</summary>
+    <p>
+      Use my itch.io username
+      <code>nnda</code>
+      <br>
+      And if possible, provide link to my itch.io
+      <a href="https://nnda.itch.io" target="_blank">profile page</a>.
+    </p>
+  </details>
+
+</div>
+`
+,
+`
+<div class="custom-accrd custom-left">
+  <details>
+    <summary>Left-aligned accordion</summary>
+    This accordion is aligned to the left.
+  </details>
+</div>
+`
+,
+`
+<div class="custom-accrd custom-right">
+  <details>
+    <summary>Right-aligned accordion</summary>
+    This accordion is aligned to the right.
+  </details>
+</div>
+`
+,
+`
+<div class="custom-accrd custom-full-w">
+  <details>
+    <summary>Full width accordion</summary>
+    This accordion takes full width of the page.
+  </details>
+</div>
+`
     ],
   },
   "callout" : {
-    desc: "Inform content warnings, additional informations, or a technical issues.",
+    desc: "Inform and highlight content warnings, additional informations, or technical issues.",
     type: "component",
     sampleHTML: [
 `<blockquote class="custom-call">
@@ -37,10 +110,23 @@ export const compBase : PitchComponentsLibrary = {
 
 </blockquote>`
 ,
+`<blockquote class="custom-call">
+
+  <h3>🐞 Note!</h3>
+  This project is still in its alpha stage. And is subject to frequent changes, and bugs.
+
+  <br>
+  <br>
+
+  <b class="custom-lb">Report bugs here</b>
+
+  And feel free to share your feedback in the comments!
+
+</blockquote>`
     ],
   },
   "info-list" : {
-    desc: "Custom class for description list element that use 2 column table layout.",
+    desc: "Custom class that style description list element to two-column layout.",
     type: "component",
     sampleHTML: [
 `<dl class="custom-info">
@@ -90,7 +176,7 @@ export const compBase : PitchComponentsLibrary = {
     ],
   },
   "input" : {
-    desc: "Represent the keyboard inputs, controls, or any buttons.",
+    desc: "Represent keyboard inputs, controls, or any buttons.",
     type: "component",
     sampleHTML: [
 `Press <kbd>Ctrl</kbd> + <kbd>A</kbd> to select all text.
@@ -106,6 +192,8 @@ Depend on your browser, <kbd>Ctrl</kbd> + <kbd>Shift</kbd> + <kbd>N</kbd> will o
 <kbd>A</kbd>,
 <kbd>S</kbd>,
 <kbd>D</kbd>
+
+<br>
 
 <kbd>Ctrl</kbd> + <kbd>Shift</kbd> + <kbd>N</kbd>`
     ],
@@ -141,6 +229,26 @@ Depend on your browser, <kbd>Ctrl</kbd> + <kbd>Shift</kbd> + <kbd>N</kbd> will o
   <small class="custom-lb">Alpha</small>
 </h3>`
 ,
+,
+`
+<h1>
+  Some Features
+  <small class="custom-lb">V2.4</small>
+</h1>
+
+<p style="max-width: 400px">
+  Hello hello, these are some of the features
+  that everyone have been waiting for!
+  We have added some
+  <i class="custom-lb">brand new</i>
+  and
+  <i class="custom-lb">epic</i>
+  features to the app!!!
+  Don't mind anything after this sentence.
+  I'm just typing whatever to pad this paragraph.
+  You can safely ignore this.
+</p>
+`
     ],
   },
   "spoiler" : {
@@ -148,13 +256,8 @@ Depend on your browser, <kbd>Ctrl</kbd> + <kbd>Shift</kbd> + <kbd>N</kbd> will o
     type: "component",
     sampleHTML: [
 `<p>
-The one who stole Nina's muffins was
-<span class="custom-hd">Mr. Snuffles</span>.
-I don't find that very surprising, to be honest.
-<span class="custom-hd">It's not the first time he committed such crime</span>.
-Let the court of law decide
-<span class="custom-hd">his inocence</span>.
-But from now on, we can sleep in peace, knowing the truth.
+  The one who stole Nina's muffins was
+  <span class="custom-hd">Mr. Snuffles</span>.
 </p>`
 ],
   },
@@ -162,32 +265,74 @@ But from now on, we can sleep in peace, knowing the truth.
     desc: "Modified default table element, that takes the full width of the page.",
     type: "component",
     sampleHTML: [
-    `<table>
-      <thead>
-        <tr>
-          <th>Rank</th>
-          <th>Name</th>
-          <th>Score</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>John Doe</td>
-          <td>900</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Godette</td>
-          <td>850</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td>Charles</td>
-          <td>700</td>
-        </tr>
-      </tbody>
-    </table>`
+`<table>
+  <thead>
+    <tr>
+      <th></th>
+      <th>Free version</th>
+      <th>Paid version</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th>Animation</th>
+      <td>❌ None</td>
+      <td>✅ Animated</td>
+    </tr>
+    <tr>
+      <th>File format</th>
+      <td>
+        .png
+      </td>
+      <td>
+        .png<br>
+        .psd<br>
+        .aseprite
+      </td>
+    </tr>
+    <tr>
+      <th>Commercial use</th>
+      <td>❌ Not allowed</td>
+      <td>✅ Allowed</td>
+    </tr>
+    <tr>
+      <th>Attribution</th>
+      <td>❌ Required</td>
+      <td>✅ Not required</td>
+    </tr>
+  </tbody>
+</table>`
+,
+`<table>
+  <thead>
+    <tr>
+      <th>Spell</th>
+      <th>Type</th>
+      <th>Min. Level</th>
+      <th>Damage</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Fire ball</td>
+      <td>🔥</td>
+      <td>2</td>
+      <td>20-25</td>
+    </tr>
+    <tr>
+      <td>Ice spear</td>
+      <td>❄️</td>
+      <td>3</td>
+      <td>10-25</td>
+    </tr>
+    <tr>
+      <td>Lightning bolt</td>
+      <td>⚡</td>
+      <td>5</td>
+      <td>15-20</td>
+    </tr>
+  </tbody>
+</table>`
     ],
   },
   "variables" : {
