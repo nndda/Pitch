10562d122ffabea77af3f2c9bc258483441eb49f
nnda <96333146+nndda@users.noreply.github.com>
2024-10-03 23:25:10 +0800

    Ignore `node_modules/`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,4 +8,10 @@ export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.strict,
   ...tseslint.configs.stylistic,
+
+  {
+    ignores: [
+      "node_modules/*",
+    ],
+  },
 );
