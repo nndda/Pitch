05e8e927deab206e56924b966c662241d1af4cbf
nnda <96333146+nndda@users.noreply.github.com>
2026-04-06 21:58:01 +0700

    config: update build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { resolve } from "path";
+
+function abs(path: string): string {
+  return resolve(__dirname, path)
+}
 
 // https://vite.dev/config/
 export default defineConfig({
-  root: "./src/",
+  root: abs("./src/"),
   plugins: [svelte()],
+  publicDir: abs("./src/public/"),
+  appType: "spa",
+  build: {
+    assetsDir: "./assets/",
+    license: true,
+
+    copyPublicDir: true,
+
+    outDir: "../dist/",
+    emptyOutDir: true,
+  },
 });
