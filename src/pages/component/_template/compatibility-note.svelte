0a8e05ff1d8157d9702cba2a5bcd46510f886391
nnda <96333146+nndda@users.noreply.github.com>
2026-05-05 21:01:38 +0700

    feat: create compatibility notes

---
+++ src/pages/component/_template/compatibility-note.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  const { inputs }: { inputs: string[] } = $props();
+</script>
+
+<p>
+  You can make this component fully compatible on all pages, by setting the following customization manually:
+</p>
+
+<ul>
+  {#each inputs as input}
+    <li>
+      <q>{input}</q>
+    </li>
+  {/each}
+</ul>
