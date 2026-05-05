f1b4002cd3555c66b7ab3f5e8dce9fb412c06814
nnda <96333146+nndda@users.noreply.github.com>
2026-05-05 21:00:58 +0700

    feat: create component reference button

---
+++ src/pages/component/_template/component-ref-button.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  const { comp }: { comp: string } = $props();
+</script>
+
+<button
+  onclick={() => {
+    (
+      document.querySelector(
+        `#sidebar [id^="comp-list-"] .comp-item label[data-comp-name="${comp}"]`
+      ) as HTMLLabelElement
+    ).click();
+  }}
+>
+  {comp}
+</button>
--- src/pages/component/_template/components.ts
+++ src/pages/component/_template/components.ts
@@ -2,3 +2,4 @@ export { default as ComponentPage } from "./page.svelte";
 export { default as CodeCSS } from "./code-css.svelte";
 export { default as CodeHTML } from "./code-html.svelte";
 export { default as Images } from "./images.svelte";
+export { default as ComponentRef } from "./component-ref-button.svelte";
