88a9c37e0e9f28aac136a7caa456d6d54fd5f9aa
nnda <96333146+nndda@users.noreply.github.com>
2026-05-05 21:19:29 +0700

    feat(callout): refactor & add in-app customization
    - Expose CSS variables overrides as in-app components' user-inputs
    - Update HTML examples/previews to be more semantic
    - Minor refactor on the heading

--- src/pages/component/components/callout.ts
+++ src/pages/component/components/callout.ts
@@ -15,4 +15,34 @@ export default {
     ],
   },
 
+  input: [
+    {
+      name: "Color",
+      var: "callout-col",
+      default: "",
+      type: "color",
+    },
+    {
+      collapse: true,
+    },
+    {
+      name: "Border",
+      var: "callout-border-col",
+      default: "",
+      type: "color",
+    },
+    {
+      name: "Background",
+      var: "callout-bg-col",
+      default: "",
+      type: "color",
+    },
+  ],
+
+  compatibleOnInputs: [
+    "callout-col",
+    "callout-border-col",
+    "callout-bg-col",
+  ]
+
 } as ComponentData;
