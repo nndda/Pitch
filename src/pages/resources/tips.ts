167bcb2e4da68d05582897555cf62a86e5dc276d
nnda <96333146+nndda@users.noreply.github.com>
2026-05-16 10:29:55 +0700

    feat: pre-generate markup tokens & add AMP entry

--- src/pages/resources/tips.ts
+++ src/pages/resources/tips.ts
@@ -13,27 +13,59 @@ function html(tag: string): string {
   return `<code>&lt;${tag}&gt;</code>`
 }
 
+function link(text: string, url: string): string {
+  return `<a target="_blank" rel="me nofollow noopener" href="${url}">${text}</a>`
+}
+
+const
+  links = {
+    AMP: link("AMP", "https://en.wikipedia.org/wiki/Accelerated_Mobile_Pages"),
+  }
+, htmlTagsStr = [
+    "input",
+    "label",
+    "a",
+    "details",
+    "b",
+    "i",
+    "strong",
+    "em",
+  ]
+, tags = {
+    ... htmlTagsStr.reduce(
+      (prev, val) => {
+        prev[val] = link(
+          html(val),
+          "https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/" + val,
+        );
+        return prev;
+      },
+      {} as Record<string, string>,
+    )
+  }
+;
+
 export const tips: Tips = {
   "HTML on itch.io": {
     icon: "fa-brands fa-html5",
     items: [
       `Blank newlines will be stripped.`,
 
-      `Input-related elements, e.g. ${html("input")}, ${html("label")}, etc. are not allowed, and will be sanitized.`,
+      `Input-related elements, e.g. ${tags.label}, ${tags.label}, etc. are not allowed, and will be sanitized.`,
 
       `Attributes ${code("id")} and ${code("name")} are not allowed.`,
 
       `Custom ${code("data-*")} attributes are not allowed.`,
 
-      `${html("a")} pointing to external links will always have the attributes: ${code(`rel="nofollow noopener"`)} and ${code(`referrerpolicy="origin"`)} added automatically`,
+      `${tags.a} pointing to external links will always have the attributes: ${code(`rel="nofollow noopener"`)} and ${code(`referrerpolicy="origin"`)} added automatically`,
 
-      `Anchor links on ${html("a")} will not work, unless you include the whole URL of the page.`,
+      `Anchor links on ${tags.a} will not work, unless you include the whole URL of the page.`,
 
       `Comment tags are not supported, and will be rendered as is.`,
 
-      `${html("b")} and ${html("i")} tags will be converted to ${html("strong")} and ${html("em")} respectively.`,
+      `${tags.b} and ${tags.i} tags will be converted to ${tags.strong} and ${tags.em} respectively.`,
 
-      `${code("name")} attribute are not supported for ${html("details")} tag, and will be stripped.`,
+      `${code("name")} attribute are not supported for ${tags.details} tag, and will be stripped.`,
     ],
   },
   "CSS on itch.io": {
@@ -42,6 +74,8 @@ export const tips: Tips = {
       `Profile pages CSS are limited to <b>5120 characters</b>.`,
 
       `It is possible to write comments in HTML, and style it by adding the ${code(".custom-*")} CSS classes, if said classes are not scoped to the actual page's content.`,
+
+      `On mobile, when visiting a devlog from Google search result, you might get served the ${links.AMP} version of the page instead. Which strips it of all the custom CSS.`
     ],
   },
 };
