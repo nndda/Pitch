cb572db713d35c1cb254ef1481992a4655832e1b
nnda <96333146+nndda@users.noreply.github.com>
2026-05-19 02:20:26 +0700

    refactor: account for comment section & remove unused layer support

--- src/scripts/compiler.ts
+++ src/scripts/compiler.ts
@@ -1,57 +1,35 @@
 import {
   runtimeData,
+  // compCheckboxCache,
 } from "../states/runtime";
 import {
   inputs,
+  projects,
   settings,
+  currentProject,
 } from "../states/storage.svelte";
-
-function layerCSS(css: string): string {
-  // if (settings.state["css.use_layer"]) {
-    if (settings.state["css.minify"]) {
-      return `@layer pitch-css{@layer b,u;@layer u{
-
-
-  /* <<< YOUR STYLING HERE >>> */
-
-
-}@layer b{${css}}}`;
-    } else {
-      return `@layer pitch-css {
-  @layer base, user;
-
-  @layer user {
-
-
-    /* <<< YOUR STYLING HERE >>> */
-
-
-  }
-
-  @layer base {
-${css}
-  }
-}`;
-    }
-  // }
-
-  // return css;
-}
 
 export function compile(): string {
   const
     inputsProps = inputs.state
+  , isProject = projects.state[currentProject.get()!].scope === "project"
 
   , cssOut: string[] = []
-  , cssMinify = settings.state["css.minify"]
-  , cssIndent = cssMinify ? "" : "  "
-  , cssNewline = cssMinify ? "" : "\n\n"
+  , minify = settings.state["css.minify"]
+  , cssIndent = minify ? "" : "  "
+  , cssNewline = minify ? "" : "\n\n"
 
   , inputStyleProps: string[] = []
   , inputStyleIncludes: string[] = []
+
+  , compsNoCommentSection: string[] = []
+  , settingNoCommentSection = settings.state["css.isolate_comment_section"]
   ;
 
-  const cssVarsNotMinified = `#wrapper {
+  cssOut.push(
+    minify
+      ? `#wrapper {--b:var(--itchio_bg_color);--b2:var(--itchio_bg2_color);--b2s:var(--itchio_bg2_sub);--t:var(--itchio_text_color);--l:var(--itchio_link_color);--br:var(--itchio_border_color);--btn:var(--itchio_button_color);--btn_f:var(--itchio_button_fg_color);--btn_s:var(--itchio_button_shadow_color)}`
+      : `#wrapper {
   --b: var(--itchio_bg_color);
   --b2: var(--itchio_bg2_color);
   --b2s: var(--itchio_bg2_sub);
@@ -61,21 +39,10 @@ export function compile(): string {
   --btn: var(--itchio_button_color);
   --btn_f: var(--itchio_button_fg_color);
   --btn_s: var(--itchio_button_shadow_color);
-}`;
-
-  cssOut.push(
-    cssMinify
-      ? `#wrapper {--b:var(--itchio_bg_color);--b2:var(--itchio_bg2_color);--b2s:var(--itchio_bg2_sub);--t:var(--itchio_text_color);--l:var(--itchio_link_color);--br:var(--itchio_border_color);--btn:var(--itchio_button_color);--btn_f:var(--itchio_button_fg_color);--btn_s:var(--itchio_button_shadow_color)}`
-      : (
-          settings.state["css.use_layer"]
-            ? cssVarsNotMinified
-              .split("\n")
-              .map(str => str + "  ")
-              .join("\n")
-            : cssVarsNotMinified
-        )
+}`
   )
 
+  // TODO: maybe iterate cached checkboxes instead?
   for (const catId in runtimeData) {
     const
       compCatData = runtimeData[catId].components
@@ -85,11 +52,12 @@ export function compile(): string {
       if (compCatData[compId].type === "item") {
         const
           compData = compCatData[compId] as ComponentRuntimeItem
+        , compManifest = compData.manifest
         ;
 
         if (compData.chkBox!.checked) {
           const
-            compInput = compData.manifest.input
+            compInput = compManifest.input
           ;
 
           if (compInput) {
@@ -100,14 +68,43 @@ export function compile(): string {
             }
           }
 
-          cssOut.push(
-            cssMinify
-              ? compData.manifest.css!.compressed
-              : compData.manifest.css!.raw
+          (
+            (
+              isProject && (
+                settingNoCommentSection || compManifest.disallowCommentSection
+              )
+            )
+              ? compsNoCommentSection
+              : cssOut
+          ).push(
+            minify
+              ? compManifest.css.compressed
+              : compManifest.css.raw
           );
         }
       }
     }
+  }
+
+  if (compsNoCommentSection.length > 0) {
+    cssOut.push(
+      minify
+        ? `.column.left_col>.formatted_description{`
+        : `.column.left_col > .formatted_description {`
+    );
+
+    for (const compCSS of compsNoCommentSection) {
+      cssOut.push(
+        minify
+          ? compCSS
+          : compCSS
+            .split("\n")
+            .map(val => "  " + val)
+            .join("\n")
+      );
+    }
+
+    cssOut.push("}");
   }
 
   if (Object.keys(inputsProps).length > 0) {
@@ -122,16 +119,9 @@ export function compile(): string {
     }
   }
 
-  const
-    cssOutStr = (
-      inputStyleProps.length > 0
-        ? [":root {", ... inputStyleProps, "}"].join(cssMinify ? "" : "\n")
-        : ""
-    ) + cssNewline + cssOut.join(cssNewline)
-  ;
-
-  return settings.state["css.use_layer"]
-    ? layerCSS(cssMinify ? cssOutStr : cssOutStr.replace(/^/gm, "    "))
-    : cssOutStr
-  ;
+  return (
+    inputStyleProps.length > 0
+      ? [":root {", ... inputStyleProps, "}"].join(minify ? "" : "\n")
+      : ""
+  ) + cssNewline + cssOut.join(cssNewline);
 }
