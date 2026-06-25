897ba2ff940601d580ad3a3e8e9c76abfb304297
nnda <git@nnda.dev>
2026-06-26 02:01:32 +0700

    comp: create `Cat Ear Divider` decoration

---
+++ src/pages/component/decorations/cat-ear-divider.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  export let data: ComponentData;
+  import { ComponentPage, CodeEditor } from "../_template/components";
+</script>
+
+<ComponentPage data={data}>
+
+  <p>
+    Customizable, animatable divider with cat ears.
+  </p>
+
+  <CodeEditor html={`
+    <div class="custom-cat-ear-div custom-anim-wiggle"></div>
+
+    <div style="font-size: 3em;" class="text-center">
+      ≈ •ω• ≈
+    </div>
+  `}/>
+
+  <h2>
+    Animations
+  </h2>
+
+  <p>
+    There are two animations available. Add one of the following class to enable them:
+  </p>
+
+  <ul>
+    <li>
+      Twitch: <code>custom-anim-twitch</code>
+    </li>
+    <li>
+      Wiggle: <code>custom-anim-wiggle</code>
+    </li>
+  </ul>
+
+  <CodeEditor html={`
+    <div class="custom-cat-ear-div"></div>
+
+    <div class="text-center">
+      No animation
+    </div>
+
+    <div class="custom-cat-ear-div custom-anim-twitch"></div>
+
+    <div class="text-center">
+      Twitch
+    </div>
+
+    <div class="custom-cat-ear-div custom-anim-wiggle"></div>
+
+    <div class="text-center">
+      Wiggle
+    </div>
+  `}/>
+
+</ComponentPage>
