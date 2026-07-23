6a110e1ff0a1afeaf8ab0c78fa43ed1aa32037b0
nnda <git@nnda.dev>
2026-07-23 10:36:14 +0700

    feat: add social links [skip ci]

--- src/viewer/viewer.svelte
+++ src/viewer/viewer.svelte
@@ -54,6 +54,48 @@
 
     <div class="flex-space"></div>
 
+    <div class="social-link-cont">
+      {#each [
+        {
+          name: "Bluesky",
+          url: "bsky.app/profile/nnda.dev",
+          icon: "bluesky"
+        },
+        {
+          name: "Patreon",
+          url: "www.patreon.com/nnda",
+          icon: "patreon"
+        },
+        {
+          name: "Ko-fi",
+          url: "ko-fi.com/nnda",
+          icon: "ko-fi"
+        },
+        {
+          name: "itch.io",
+          url: "nnda.itch.io/",
+          icon: "itch-io"
+        },
+        {
+          name: "GitHub",
+          url: "github.com/nndda/Pitch",
+          icon: "github"
+        },
+      ] as { name, url, icon }}
+        <a
+          target="_blank"
+          href="https://{url}"
+          rel="nofollow noopener"
+          class="social-link custom-tip button"
+        >
+          <i class="fa-brands fa-{icon}"></i>
+          <span class="custom-tip-content">
+            {name}
+          </span>
+        </a>
+      {/each}
+    </div>
+
     <input
       type="checkbox"
       class="toggle"
