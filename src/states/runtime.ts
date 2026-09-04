d1c297f440c3c025e61a8d5b8c9465ee882b49bf
nnda <git@nnda.dev>
2026-09-04 16:16:59 +0700

    refactor: migrate component catalogue APIs from sidebar to runtime
    [skip ci]

--- src/states/runtime.ts
+++ src/states/runtime.ts
@@ -1,5 +1,15 @@
 import { slug } from "../scripts/slugify";
-import { getProject } from "../storage/db";
+import { getProject, projectUpdate } from "../storage/db";
+import { toast, toastErr } from "../scripts/toast";
+import { goToPage } from "./page.svelte";
+import { copyStr } from "../scripts/copy";
+import { compile } from "../scripts/compiler";
+
+import {
+  updateCatSelectionState,
+  syncCompCheckedState,
+  syncCompGroupItemsClass,
+} from "../sidebar/sidebar";
 
 import compPagesEntry from "../pages/component/imports";
 
@@ -53,7 +63,8 @@ export function runtimeDataInit() {
     for (const page in compPagesEntry[cat]) {
       const
         compData = compPagesEntry[cat][page]
-      , compId = slug(compData.nameDisplay ?? compData.name)
+      , compHumanName = compData.nameDisplay ?? compData.name
+      , compId = slug(compHumanName)
       ;
 
       if (compData.scopes !== "group-only") {
@@ -88,8 +99,148 @@ export function runtimeDataInit() {
               isHacky: compData.tags?.includes("hacky") ?? false,
               isExperimental: compData.tags?.includes("experimental") ?? false,
 
+              api: {
+
+                openPage() {
+                  goToPage({
+                    title: compHumanName,
+                    content: compData.page,
+
+                    componentData: runtimeData[cat].components[compId] as ComponentRuntimeItem,
+                  });
+                },
+
+                async toggleInclude(included) {
+                  const
+                    compRuntimeData = runtimeData[cat].components[compId] as ComponentRuntimeItem
+                  ;
+
+                  let
+                    updated = false
+                  ;
+
+                  try {
+                    const
+                      // Update the project's DB selection state
+                      updates = await projectUpdate(proj => {
+                        if (!(cat in proj.components)) {
+                          proj.components[cat] = {}
+                        }
+
+                        // Delete the key if not checked (not selected), instead of flagging it to false
+                        // if (checked) {
+                        //   proj.components[catId][compId] = true;
+                        // } else {
+                        //   delete proj.components[catId][compId];
+                        // }
+
+                        // Flag false explicitly if not checked, rather than deleting the key
+                        proj.components[cat][compId] = included;
+                      })
+                    ;
+
+                    // If the DB gets updated...
+                    if (updates > 0) {
+
+                      updateCatSelectionState(cat);
+
+                      // Auto copy
+                      if (project?.app.settings.app.autoCopy) {
+                        copyStr(
+                          await compile(),
+                        );
+                      }
+
+                      // Notify the user about the component's selection state
+                      toast(
+                        included ?
+                          // NOTE: not sure which icon is better :/
+                          // `<i class="fa-solid fa-check-to-slot"></i> <b>${compHumanName}</b> added` :
+                          `<i class="fa-solid fa-square-check"></i> <b>${compHumanName}</b> added` :
+                          `<i class="fa-solid fa-border-none"></i> <b>${compHumanName}</b> removed`
+                      );
+
+                      updated = true;
+
+                    } else {
+
+                      // If nothing gets updated...
+                      // Notify the user
+                      toastErr(
+                        included ?
+                          `Failed to remove <b>${compHumanName}</b>` :
+                          `Failed to add <b>${compHumanName}</b>`
+                      );
+
+                    }
+
+                  } catch (err) {
+                    toastErr(`${err}`);
+                  }
+
+                  if (updated) {
+
+                    syncCompCheckedState(cat, compRuntimeData, included);
+
+                  } else {
+
+                    // Revert back selection state if failed to update
+                    compRuntimeData.chkBox.checked = !included;
+
+                  }
+                },
+
+                async toggleFavourite(faved) {
+                  const
+                    compRuntimeData = (runtimeData[cat].components[compId] as ComponentRuntimeItem)
+                  ;
+
+                  compRuntimeData.li!.classList.toggle("is-faved", faved);
+
+                  if (compRuntimeData.group) {
+                    syncCompGroupItemsClass(
+                      runtimeData[cat].components[compRuntimeData.group],
+                      "is-faved",
+                    );
+                  }
+
+                  try {
+                    const
+                      // Update the project's DB faved state
+                      updates = await projectUpdate(proj => {
+                        if (faved) {
+                          proj.faves[compId] = true;
+                        } else {
+                          delete proj.faves[compId];
+                        }
+                      })
+                    ;
+
+                    // If the DB gets updated...
+                    if (updates > 0) {
+                      // Notify the user about the component's favourited state
+                      toast(
+                        faved ?
+                          `<i class="fa-solid fa-star"></i> Added <b>${compHumanName}</b> to favourites` :
+                          `<i class="fa-regular fa-star"></i> Removed <b>${compHumanName}</b> from favourites`
+                      );
+                    } else {
+                      // If nothing gets updated...
+                      // Notify the user
+                      toastErr(
+                        faved ?
+                          `Failed to remove <b>${compHumanName}</b> from favourites` :
+                          `Failed to add <b>${compHumanName}</b> to favourites`
+                      );
+                    }
+                  } catch (err) {
+                    toastErr(`${err}`)
+                  }
+                }
+              },
             }
           ),
+
 
         } as ComponentRuntimeItem;
 
