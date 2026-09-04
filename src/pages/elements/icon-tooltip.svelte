6a34aaffaf90335557faae0936c13ee42b8f3c52
nnda <git@nnda.dev>
2026-09-04 16:10:09 +0700

    feat: abstract tooltip icon element

---
+++ src/pages/elements/icon-tooltip.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  const {
+    icon,
+    tooltip,
+
+    forId,
+
+    elClass,
+  }: {
+    icon: string,
+    tooltip: string,
+
+    forId?: string,
+
+    elClass?: string,
+  } = $props();
+</script>
+
+{#if forId}
+  <label class="custom-tip {elClass}" for={forId}>
+    <i class="{icon}"></i>
+    <span class="custom-tip-content">
+      {tooltip}
+    </span>
+  </label>
+{:else}
+  <span class="custom-tip {elClass}">
+    <i class="{icon}"></i>
+    <span class="custom-tip-content">
+      {tooltip}
+    </span>
+  </span>
+{/if}
