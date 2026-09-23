740a63dc078052e1358252e8f4e6d3a87852ac31
nnda <git@nnda.dev>
2026-09-23 23:30:00 +0700

    chore: declare types globally

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,11 +1,20 @@
-declare module "*?css-component" {
-  const value: CSSData;
-  export default value;
+import type { Component } from "svelte";
+
+namespace global {
+  declare module "*?css-component" {
+    const value: CSSData;
+    export default value;
+  }
+
+  declare module "*.html" {
+    const value: Component;
+    export default value;
+  }
+
+  declare module "not-a-toast";
+
+  declare type RecordString = Record<string, string>;
+  declare type RecordBoolean = Record<string, boolean>;
+  declare type RecordUserInput = Record<string, ComponentUserInputValue>;
+  declare type RecordUserPreviewCodes = Record<string, { html: string, css: string, }>;
 }
-
-declare module "not-a-toast";
-
-declare type RecordString = Record<string, string>;
-declare type RecordBoolean = Record<string, boolean>;
-declare type RecordUserInput = Record<string, ComponentUserInputValue>;
-declare type RecordUserPreviewCodes = Record<string, { html: string, css: string, }>;
